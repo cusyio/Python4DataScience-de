d1f32c3a38efbbf6c4e2299283bc550ed96c8762
veit <veit@cusy.io>
2025-08-04 11:13:52 +0200

    :wrench: Add sphinx sitemap

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx_copybutton",
     "sphinx_inline_tabs",
+    "sphinx_sitemap",
     "sphinxcontrib.cairosvgconverter",
     "sphinxext.opengraph",
 ]
@@ -252,3 +253,13 @@ def setup(app):
 # -- graphviz configuration --------------------------------------------------
 
 graphviz_output_format = "svg"
+
+# -- sitemap configuration ---------------------------------------------------
+
+sitemap_url_scheme = "{link}"
+sitemap_excludes = [
+    "404.html",
+    "search.html",
+    "genindex.html",
+]
+sitemap_show_lastmod = True
